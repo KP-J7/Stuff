c6a43c624cfb4c00e7fb88c70007806c1d9da667
KP-J7 <pospelovalara78@gmail.com>
2026-09-20 21:47:57 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include<cmath>
+#include<numbers>
+using namespace std;
+
+int main() {
+    int a,b;
+    cin>>a;
+    cin>>b;
+    cout<<2*pow(sin(3*3.14-2*a),2)*pow(cos(5*3.14-2*a),2)<<endl;
+    cout<<0.25-0.25*sin(2.5*3.14-8*a)<<endl;
+}
