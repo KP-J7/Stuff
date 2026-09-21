12e0730592ea39c3fa0b77d08740a2c820a561b6
KP-J7 <pospelovalara78@gmail.com>
2026-09-21 22:43:50 +0300

    Add files via upload

---
+++ v10.c
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main(){
+    int a;
+    cin>>a;
+    cout<<(sin(3.14/2+3*a))/(1-sin(3*a-3.14))<<endl;
+    cout<<1/(tan(1.25*3.14+1.5*a))<<endl;
+}
