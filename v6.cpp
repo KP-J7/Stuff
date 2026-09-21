c9db9c1d7c7c7a1b9ff615c4bac40bd83c8909e1
KP-J7 <pospelovalara78@gmail.com>
2026-09-21 22:29:51 +0300

    Add files via upload

---
+++ v6.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main(){
+    int a;
+    cin>>a;
+    cout<<cos(a)+cos(2*a)+cos(6*a)+cos(7*a)<<endl;
+    cout<<4*cos(a/2)*cos(2.5*a)*cos(4*a)<<endl;
+}
