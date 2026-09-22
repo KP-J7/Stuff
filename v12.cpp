bed4c2607130556ca90cde2bfbd2f4c0a5b64a53
KP-J7 <pospelovalara78@gmail.com>
2026-09-22 22:06:05 +0300

    Add files via upload

---
+++ v12.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int a;
+    cin>>a;
+    cout<<((sin(4*a))/(1+cos(4*a)))*((cos(2*a))/(1+cos(2*a)))<<endl;
+    cout<<1/tan(1.5*3.14-a)<<endl;
+}
