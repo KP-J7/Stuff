941a3cad0ed1494e4b08e24c378ffd6690d6eb27
KP-J7 <pospelovalara78@gmail.com>
2026-09-22 22:12:37 +0300

    Add files via upload

---
+++ 14.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int a;
+    cin>>a;
+    cout<<(cos(a)+sin(a))/(cos(a)-sin(a))<<endl;
+    cout<<tan(2*a)+1/cos(2*a)<<endl;
+}
